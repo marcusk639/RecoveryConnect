6e46074720b1a502a8178e6d089f3a4783212683
Marcus Klein <marcusk639@gmail.com>
2025-04-06 17:02:43 -0700

    first successful deploy of find meetings function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,11 @@
-import * as functions from "firebase-functions";
-import {getNarcoticsAnoymousMeetings, getAll12StepMeetings, getCustomMeetings, getAlcoholicsAnonymousMeetings} from "./utils/meetings";
-import {Meeting, MeetingSearchCriteria} from "./entities/Meeting";
+import * as functions from "firebase-functions/v2";
+import {
+  getNarcoticsAnoymousMeetings,
+  getAll12StepMeetings,
+  getCustomMeetings,
+  getAlcoholicsAnonymousMeetings,
+} from "./utils/meetings";
+import { Meeting, MeetingSearchCriteria } from "./entities/Meeting";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -16,14 +21,14 @@ import {Meeting, MeetingSearchCriteria} from "./entities/Meeting";
  * }
  */
 interface MeetingSearchInput {
-  filters: {
+  filters?: {
     date: string;
     location: {
       lat: number;
       lng: number;
     };
-    day: string;
-    type: MeetingTypeFilters;
+    day?: string;
+    type?: MeetingTypeFilters;
   };
 
   criteria?: MeetingSearchCriteria;
@@ -37,58 +42,56 @@ export type MeetingTypeFilters =
   | "Custom"
   | "all"
   | "Celebrate Recovery";
-
-export const findMeetings = functions.https.onCall(
-    async (data: MeetingSearchInput, context) => {
-      const meetingPromises: Promise<Meeting[]>[] = [];
-      const start = Date.now();
-      try {
-        functions.logger.info("FIND MEETING Filters", data.filters);
-        if (data.filters && data.filters.type && data.filters.type !== "all") {
-          if (data.filters.type === "AA") {
-            const aaMeetings = getAlcoholicsAnonymousMeetings(
-                data.filters.location,
-                data.criteria
-            );
-            meetingPromises.push(aaMeetings);
-          }
-          if (data.filters.type === "NA") {
-            const naMeetings = getNarcoticsAnoymousMeetings(
-                data.filters.location,
-                data.criteria,
-                data.filters.day
-            );
-            meetingPromises.push(naMeetings);
-          }
-          if (data.filters.type === "Custom") {
-            const customMeetings = getCustomMeetings(
-                data.filters.location,
-                data.criteria
-            );
-            meetingPromises.push(customMeetings);
-          }
-        } else {
-          const all12StepMeetings = getAll12StepMeetings(
-              data.filters.location,
-              data.criteria,
-              data.filters.day
-          );
-          meetingPromises.push(all12StepMeetings);
-        }
-      } catch (error) {
-        functions.logger.info("SOMETHING WENT WRONG", error);
+export const findMeetings = functions.https.onCall(async (request) => {
+  const data = request.data as MeetingSearchInput;
+  const meetingPromises: Promise<Meeting[]>[] = [];
+  const start = Date.now();
+  try {
+    functions.logger.info("FIND MEETING Filters", data.filters);
+    if (data.filters && data.filters.type && data.filters.type !== "all") {
+      if (data.filters.type === "AA") {
+        const aaMeetings = getAlcoholicsAnonymousMeetings(
+          data.filters.location,
+          data.criteria
+        );
+        meetingPromises.push(aaMeetings);
       }
-      const results = await Promise.all(meetingPromises);
-      const meetings: Meeting[] = [];
-      for (const meetingSet of results) {
-        meetings.push(...meetingSet);
+      if (data.filters.type === "NA") {
+        const naMeetings = getNarcoticsAnoymousMeetings(
+          data.filters.location,
+          data.criteria,
+          data.filters.day
+        );
+        meetingPromises.push(naMeetings);
       }
-      const end = Date.now();
-      functions.logger.info(
-          "Meeting retrieval took",
-          (end - start) / 1000,
-          "seconds"
+      if (data.filters.type === "Custom") {
+        const customMeetings = getCustomMeetings(
+          data.filters.location,
+          data.criteria
+        );
+        meetingPromises.push(customMeetings);
+      }
+    } else {
+      const all12StepMeetings = getAll12StepMeetings(
+        data.filters.location,
+        data.criteria,
+        data.filters.day
       );
-      return meetings;
+      meetingPromises.push(all12StepMeetings);
     }
-);
+  } catch (error) {
+    functions.logger.info("SOMETHING WENT WRONG", error);
+  }
+  const results = await Promise.all(meetingPromises);
+  const meetings: Meeting[] = [];
+  for (const meetingSet of results) {
+    meetings.push(...meetingSet);
+  }
+  const end = Date.now();
+  functions.logger.info(
+    "Meeting retrieval took",
+    (end - start) / 1000,
+    "seconds"
+  );
+  return meetings;
+});
